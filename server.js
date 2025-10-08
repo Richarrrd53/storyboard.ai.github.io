44fabd56747513faf119ee4f8c4cecd76b97fc81
unknown <orgalorg8732@gmail.com>
2025-10-08 14:32:50 +0800

    resolve server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require("fs");
-const { marked } = require('marked'); // 已調整為標準的 CommonJS require 語法
+const { marked } = import('marked'); // 已調整為標準的 CommonJS require 語法
 
 const { GoogleGenAI } = require("@google/genai");
 
